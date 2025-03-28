edd915d3b98be47c8b49fd21cef78a1a36404719
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-28 09:35:51 -0300

    add new const clickedKey to add a class and a setTimeout

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,11 +1,21 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
+
+
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
 
     audio.src = `src/tunes/${key}.wav`
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+
+    clickedKey.classList.add("active");
+
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 
 }
 
