a91b22e6a00debc07eca4b3fedb07541913517bc
Chrystine Martins <chrystine.martins@outlook.com>
2025-03-28 09:40:21 -0300

    add new const mapedKey and if mapedKey

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
 
+let mapedKeys = [];
 
 let audio = new Audio("src/tunes/a.wav");
 
@@ -8,6 +9,8 @@ const playTune = (key) => {
 
     audio.src = `src/tunes/${key}.wav`
     audio.play();
+
+    console.log(mapedKeys);
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
 
@@ -24,13 +27,21 @@ const playTune = (key) => {
 pianoKeys.forEach((key) => {
     //console.log(key.dataset.key);
 
-    key.addEventListener('click', () => playTune(key.dataset.key))
+    key.addEventListener('click', () => playTune(key.dataset.key));
+
+    mapedKeys.push(key.dataset.key);
 })
 
 
 document.addEventListener("keydown", (e) => {
 
-    playTune(e.key);
-    //console.log(e.key)
+    if (mapedKeys.includes(e.key)) {
+
+        playTune(e.key);
+        //console.log(e.key)
+
+    }
+
+
 
 })
